50c8cc8f265befb36cf25b2504ad53b9219f0619
Rohini Rao <rohinirao2112@gmail.com>
2022-02-13 11:43:13 +0530

    tests for campaign [WIP]

--- tests/campaign.test.ts
+++ tests/campaign.test.ts
@@ -1,5 +1,15 @@
-import { Signer } from "ethers";
+import { Contract, ContractFactory, Signer } from "ethers";
+import { ethers } from "hardhat";
+
+let owner: Signer;
+let fetchedAccounts: Signer[];
+let factory: ContractFactory;
+let contract: Contract;
 
 beforeEach(async () => {
-    
+    [owner, ...fetchedAccounts] = await ethers.getSigners();
+
+    factory = await ethers.getContractFactory("CampaignFactory")
+    contract = await factory.deploy()
 })
+
