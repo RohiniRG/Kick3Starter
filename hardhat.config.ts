11bec8338ecd6d17a29e8144f8eb384203386963
Rohini Rao <rohinirao2112@gmail.com>
2022-03-02 22:00:18 +0530

    Finished deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,14 @@
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
   solidity: "0.7.3",
+    networks: {
+     rinkeby: {
+       url: `${process.env.RINKEBY_API}`, 
+       accounts: [`${process.env.PRIVATE_KEY}`] // add the account that will deploy the contract (private key)
+      },
+    }  
 };
---
+++ scripts/deploy.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const CampaignFactory = await ethers.getContractFactory("CampaignFactory");
+  const campaignFactory = await CampaignFactory.deploy();
+  console.log("Campaign address:", campaignFactory.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+  
