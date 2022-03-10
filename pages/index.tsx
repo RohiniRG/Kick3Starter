255cbbd1d2d919d03979f79c87af27f6e9e1969d
Rohini Rao <rohinirao2112@gmail.com>
2022-03-10 07:26:17 +0530

    landing page menu added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Contract, ethers} from 'ethers';
+import { Contract, ethers } from 'ethers';
 import { TransactionResponse, Provider } from "@ethersproject/abstract-provider";
 import CampaignFactory from '../artifacts/contracts/campaign.sol/CampaignFactory.json'
 import web3Provider from '../scripts/web3_provider';
-import { Header, Grid, Image, Button, Icon, Popup } from 'semantic-ui-react'
+import { Header, Grid, Image, Button, Icon, Popup, Container, Menu, Form, Message, GridColumn } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 // import { TransactionResponse } from '@ethersproject/providers';
 
@@ -21,12 +21,6 @@ class CampaignList extends Component<any> {
         return { campaigns };
     }
 
-    // async getTransactionTime (address: string): Promise<any> {
-    //     await web3Provider.getBlockNumber().then((blockNumber: number) => {console.log(blockNumber)});
-    //     // const timest: number  = (await web3Provider.getBlock(9)).timestamp
-    //     // return timest;
-    // }
-
     hexShortner = (address): String => {
         return address.substr(0, 5) + " ... " + address.substr(address.length - 5);
     }
@@ -34,54 +28,99 @@ class CampaignList extends Component<any> {
     copyToClipboard = (address) => {
         this.setState({ copied: true })
         setTimeout(() => {
-          this.setState({ copied: false })
+            this.setState({ copied: false })
         }, 1000);
         return navigator.clipboard.writeText(address);
-    }    
+    }
 
     campaignsCardGroup(): JSX.Element {
         const items: JSX.Element = this.props.campaigns.map((campaignAddress: string, i: number) => {
-        return (
-            <Grid.Column mobile={16} tablet={8} computer={4}>
-                <Grid.Row>
-                    <Image
-                        floated='left'
-                        size='small'
-                        src={`https://avatars.dicebear.com/api/jdenticon/${campaignAddress}.svg`} />
-                    <Header as="h2">Campaign {(i+1).toString()}</Header>                    
-                </Grid.Row>
-                <div onClick={() => this.copyToClipboard(campaignAddress)}>
-                    <p style={{ 'color': 'grey', 'display': 'inline', 'fontSize': '17px'}}>
-                        {this.hexShortner(campaignAddress)}
-                    </p>
-                    <Popup 
-                        trigger={<Icon color='grey' name='copy outline' />}
-                        content='Copy to clipboard'
-                        position='bottom center'
-                    />
-                </div>
-                <br />
-                <br />
-                <br />
-                <Button basic size="medium">
-                    View details &raquo;
-                </Button>
-            </Grid.Column>
-        )
+            return (
+                <Grid.Column mobile={16} tablet={8} computer={4}>
+                    <Grid.Row>
+                        <Image
+                            floated='left'
+                            size='small'
+                            src={`https://avatars.dicebear.com/api/jdenticon/${campaignAddress}.svg`} />
+                        <Header as="h2">Campaign {(i + 1).toString()}</Header>
+                    </Grid.Row>
+                    <div onClick={() => this.copyToClipboard(campaignAddress)}>
+                        <p style={{ 'color': 'grey', 'display': 'inline', 'fontSize': '17px' }}>
+                            {this.hexShortner(campaignAddress)}
+                        </p>
+                        <Popup
+                            trigger={<Icon color='grey' name='copy outline' />}
+                            content='Copy to clipboard'
+                            position='bottom center' />
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <Button basic size="medium">
+                        View details &raquo;
+                    </Button>
+                </Grid.Column>
+            )
         })
         return items;
     }
 
     render(): JSX.Element {
         return (
-            <div>
+            <>
+                <Grid padded>
+                    <Grid.Column>
+                        <Menu inverted borderless fluid fixed="top">
+                            <Menu.Item header as='h1'>Kick3Starter</Menu.Item>
+                            <Menu.Menu position="right">
+                                <Menu.Item>
+                                    <Button positive size='large'>
+                                        <p style={{ 'fontSize': '17px' }}>
+                                            Create Campaign
+                                        </p>
+                                    </Button>
+                                </Menu.Item>
+                                <Menu.Item>
+                                    <Button color='blue' size='large'>
+                                        <p style={{ 'fontSize': '17px' }}>
+                                            Contribute
+                                        </p>
+                                    </Button>
+                                </Menu.Item>
+                            </Menu.Menu>
+                        </Menu>
+                    </Grid.Column>
+                </Grid>
+                <Grid>
+                    <Message size="massive" floating>
+                        <br />
+                        <Header textAlign='center' size="huge" as="h1">
+                            Let's decentralise creativity!
+                        </Header>
+                        <p style={{ 'textAlign': 'center' }}>
+                            Kick3Starter is a decentralized campaign platform that allows creators to help creators in bringing their projects to life.
+                            Creators will have the opportunity to work with full control over their projects and with people who are constantly willing to support them.
+                            Come, let's make our ideas into reality!
+                        </p>
+                        <br />
+                        <Grid padded centered>
+                            <Button positive size='huge'>
+                                <p style={{ 'fontSize': '20px' }}>
+                                    Create Campaign
+                                </p>
+                            </Button>
+                        </Grid>
+
+                    </Message>
+                </Grid>
+
                 <h1>
                     Here are the list of campaigns
                 </h1>
-                <Grid>
+                <Grid padded columns={3} centered={true}>
                     {this.campaignsCardGroup()}
                 </Grid>
-            </div>
+            </>
         )
     }
 }
